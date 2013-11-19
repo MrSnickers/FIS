04c7cadc69a1fd4e375ddbc4f6e29095be9c8005
Sterling Walker <lazarus_star@hotmail.com>
2013-11-19 18:41:27 -0500

    TDD iteration homework

---
+++ TDD_iteration/square_arrays_solution.rb
@@ -0,0 +1,9 @@
+# Instructions:
+# Return an array that contains the squares of all elements in an array?
+
+def square_array(some_array)
+  array_squared = []
+some_array.each do |item|
+  array_squared << item*item
+end
+end

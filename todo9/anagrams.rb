2a306faf3b64f63143dba17507ea3c4628e56290
Sterling Walker <lazarus_star@hotmail.com>
2013-11-22 13:17:20 -0500

    School provided tests pass

--- todo9/anagrams.rb
+++ todo9/anagrams.rb
@@ -8,33 +8,14 @@ def initialize(word)
   @word = word
 end
 
-# def match(test_array)
-# test_array = test_array.abbreviate
-# return test_array if test_array.nil
-
-#   index_counter = 0
-#   test_array.each do |item|
-#     item.each do |letter|
-#       word.each do |test_letter|
-#         letter_index = 0
-#         letter_index += 1 if letter != test_letter
-#         break if letter_index > item.length
-          
-#         end
-#         index_counter += 1
-#       end
-#     end
-
-# return test_array[index]
-
-# end
-
-def match
-  abbreviated_array =[]
+def match(test_array)
+  anagram_array = []
   test_array.each do |item|
-    abbreviated_array << item.downcase if item.length == word.length
-    abbreviated_array
+    if item.chars.sort.join == word.chars.sort.join
+      anagram_array << item
+    end
   end
+   anagram_array
 end
 
 

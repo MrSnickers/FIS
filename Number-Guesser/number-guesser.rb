ad10b59df1b68696f21b4f1535c88bb680510912
Sterling Walker <lazarus_star@hotmail.com>
2013-11-24 15:35:05 -0500

    Tests pass now

--- Number-Guesser/number-guesser.rb
+++ Number-Guesser/number-guesser.rb
@@ -6,16 +6,22 @@
 # Should receive input from the user
 # Should tell the user if they were correct or incorrect
 
+def guess(answer, standard)
+      if answer == standard
+          puts "That's it!"
+          return true
+    else
+          puts "Nope.  I was thinking of #{standard}."
+          return false
+    end
+end
+
+
 puts "Please guess a number between 1 and 100."
 input = gets.chomp
 standard = rand(1..100)
+guess(input, standard)
 
 
-def guess(answer, standard)
-  if input == standard
-  puts "That's it!"
-  return true
-else
-  puts "Nope.  I was thinking of #{standard}."
-  return false
-end
+
+

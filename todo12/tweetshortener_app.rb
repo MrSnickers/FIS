34102eb0cd7ad1895bbc439c92159c4d4547882b
Sterling Walker <lazarus_star@hotmail.com>
2013-12-02 10:04:20 -0500

    Shortens tweets now

--- todo12/tweetshortener_app.rb
+++ todo12/tweetshortener_app.rb
@@ -1,7 +1,8 @@
 ###TWEET SHORTENER
 
-tweet = "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right? I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real."
-
+tweet_array = ["Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!", "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?", "I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real.", "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!"]
+truncated_array = []
+tweet_array.each do |tweet|
   if tweet.size > 140
     word_array = tweet.split(" ")
     word_array.each_index do |i|
@@ -13,5 +14,9 @@ tweet = "OMG you guys, you won't believe how sweet my kitten is. My kitten is li
       word_array[i] = "@" if word == "at"
       word_array[i] = "&" if word == "and"
     end
-    puts word_array.join(" ")
+    truncated_array << word_array.join(" ")[0..139]
   end
+end
+
+puts truncated_array
+

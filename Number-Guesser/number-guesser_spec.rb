4231650763699346b5097cfb39bf81b44934b8ff
Sterling Walker <lazarus_star@hotmail.com>
2014-01-03 09:44:32 -0500

    moved rspec helper code into .rspec file.

--- Number-Guesser/number-guesser_spec.rb
+++ Number-Guesser/number-guesser_spec.rb
@@ -3,16 +3,6 @@
 require './number_guesser_class.rb'
 require 'mocha/setup'
 
-RSpec.configure do |config|
-  # Use color in STDOUT
-  config.color_enabled = true
-
-  # Use color not only in STDOUT but also in pagers and files
-  config.tty = true
-
-  # Use the specified formatter
-  config.formatter = :documentation # :progress, :html, :textmate
-end
 
 describe "#standard" do
   it "should create a standard number between 1 and 100" do

c61962a3ba8506a8220961d8d19d5d4d3d603570
Sterling Walker <lazarus_star@hotmail.com>
2014-01-08 09:24:21 -0500

    Initial commit

---
+++ todo31/longest_palindrome.rb
@@ -0,0 +1,8 @@
+### LONGEST PALINDROME METHOD
+# A palindrome is a string that is written the same forward as it is in reverse. Write a method to return the longest palindrome in a given string
+ 
+def longest_palindrome
+ 
+end
+ 
+test_palindrome = "afbbbfjdjklgdfdhfdkjfffhhfffjkdfhdhkyejejfjkd" #=> "dhfdkjfffhhfffjkdfhd"

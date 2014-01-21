1ba670e5835533beb9af0a178da3ec0cd48e71ce
Sterling Walker <lazarus_star@hotmail.com>
2014-01-21 09:22:00 -0500

    Initial commit

---
+++ todo41/spec/fizzbuzzSpec.js
@@ -0,0 +1,18 @@
+describe( "#fizzbuzz", function() {
+  it("should return 'fizzbuzz' when number is divisble by 15", function() {
+    expect(fizzbuzz(75)).toBe("fizzbuzz");
+  });      
+
+  it("should return 'buzz' when number is divisble by 5, but not 15", function() {
+    expect(fizzbuzz(10)).toBe("buzz");
+  });
+
+  it("should return 'buzz' when number is divisble by 5, but not 15", function() {
+    expect(fizzbuzz(9)).toBe("fizz");
+  });
+
+  it("should return 'buzz' when number is divisble by 5, but not 15", function() {
+    expect(fizzbuzz(13)).toBe("13");
+  });
+
+});

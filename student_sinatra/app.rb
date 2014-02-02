be13d1475e7aa5d1a607102ece463df60f37c3de
Sterling Walker <lazarus_star@hotmail.com>
2014-02-02 09:36:00 -0500

    Modified views

--- student_sinatra/app.rb
+++ student_sinatra/app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/thing_class.rb'
 
 # Why is it a good idea to wrap our App class in a module?
 module StudentSite
@@ -7,7 +8,13 @@ module StudentSite
     get '/hello-world' do
       one_through_twenty = Array.new(20){|i| i+1}
       @random_numbers = one_through_twenty
+      @thing = Thing.new("Ashley", 6)
       erb :hello
+    end
+
+    get '/artists' do
+      @artist_array = ["Billy Collins", "Patti Smith"]
+      erb :artists
     end
 
 
---
+++ student_sinatra/lib/thing_class.rb
@@ -0,0 +1,10 @@
+class Thing
+
+attr_accessor :name, :num
+
+  def initialize(name, num)
+    @name = name
+    @num = num
+  end
+
+end

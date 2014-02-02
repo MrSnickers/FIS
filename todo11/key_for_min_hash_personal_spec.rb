9e977f443b27c9d259812a09a13b965536a01d37
Sterling Walker <lazarus_star@hotmail.com>
2014-02-02 10:27:02 -0500

    Refactor

--- todo11/key_for_min_hash_personal_spec.rb
+++ todo11/key_for_min_hash_personal_spec.rb
@@ -1,18 +1,7 @@
 ### PERSONAL SPEC FOR KEY FOR MIN HASH
 
  
-RSpec.configure do |config|
-  # Use color in STDOUT
-  config.color_enabled = true
- 
-  # Use color not only in STDOUT but also in pagers and files
-  config.tty = true
- 
-  # Use the specified formatter
-  config.formatter = :progress # :progress, :html, :textmate
-end
-
-require_relative "./key_for_min_hash.rb"
+require "./key_for_min_hash_refactor.rb"
  
 describe "#key_for_min_value" do
   it "should return the key of the smallest hash value" do
--- todo11/key_for_min_hash_refactor.rb
+++ todo11/key_for_min_hash_refactor.rb
@@ -1,10 +1,11 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+
 def key_for_min_value(hash)
-  value_only_array = []
+  min = nil
   hash.each_pair do |key, value|
-    value_only_array << value
+    min ||= value
+    min = value if value < min
   end
-  value_only_array.sort!
-  hash.key(value_only_array[0])
+  hash.key(min)
 end
